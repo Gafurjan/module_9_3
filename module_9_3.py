a46e007bb36db5b1cb528f84e7d4a9cd49129693
Gafurjan <48514749+Gafurjan@users.noreply.github.com>
2024-11-06 21:55:56 +0300

    Add files via upload

---
+++ module_9_3.py
@@ -0,0 +1,14 @@
+from operator import index
+
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютеры']
+
+first_result = (len(x)-len(y) for x, y  in zip(first, second) if len(x) != len(y))
+i = 0
+j = 0
+
+# second_result = (print(first[i], "-", second[j]) for i in range(0, len(first)) for j in range(0, len(second)) if i == j)
+second_result = (len(first[i])<len(second[j]) for i in range(0, len(first)) for j in range(0, len(second)) if i == j)
+print(list(second_result))
+
+
