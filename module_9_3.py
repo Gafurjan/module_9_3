0be719306b7b3f2ec64cf828993bac8de3bcd6bd
Gafurjan <48514749+Gafurjan@users.noreply.github.com>
2024-11-07 08:00:22 +0300

    Update module_9_3.py
    new version

--- module_9_3.py
+++ module_9_3.py
@@ -8,7 +8,9 @@ i = 0
 j = 0
 
 # second_result = (print(first[i], "-", second[j]) for i in range(0, len(first)) for j in range(0, len(second)) if i == j)
-second_result = (len(first[i])<len(second[j]) for i in range(0, len(first)) for j in range(0, len(second)) if i == j)
+#second_result = (len(first[i])<len(second[j]) for i in range(0, len(first)) for j in range(0, len(second)) if i == j)
+second_result = (len(x)<len(second[j]) for x in first for j in range(0, len(second)) if first.index(x) == j)
+print(list(first_result))
 print(list(second_result))
 
 
